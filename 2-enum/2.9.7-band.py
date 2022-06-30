d386f8729fd4c02bb876a630354e03b38ea9a64b
Stas Ukolov <ukoloff@gmail.com>
2022-06-30 21:17:12 +0500

    Solve first task

---
+++ 2-enum/2.9.7-band.py
@@ -0,0 +1,32 @@
+def bands(n, m, head=''):
+    """
+    Расстановка фишек. Имеется полоса размера 1×n,
+    разбитая на единичные клетки.
+    Нужно расставить в клетках полосы m фишек,
+    чтобы никакие две фишки не стояли в соседних клетках.
+    Выведите все возможные расстановки.
+
+    Входные данные: Натуральные числа nn и mm.
+    """
+    if m == 0:
+        if n >= 0:
+            yield head + '.' * n
+        return
+    if n < 2 * m - 1:
+        return
+    if n == 2 * m - 1:
+        yield head + ('.*' * m)[1:]
+        return
+    yield from bands(n - 2, m - 1, head + '*.')
+    yield from bands(n - 1, m, head + '.')
+
+# print(*bands(5, 2), sep='\n')
+# z = bands(5, 2)
+# for i in range(6):
+#   next(z)
+# # next(z)
+# print(next(z))
+
+from itertools import islice
+print(*islice(bands(7, 3), 6, 7))
+# print(*bands(7, 3), sep='\n')
