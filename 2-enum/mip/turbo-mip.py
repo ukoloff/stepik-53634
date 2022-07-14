82af482e0859bc2587748602ac926e078e76f735
Stas Ukolov <ukoloff@gmail.com>
2022-07-14 22:23:37 +0500

    Lazy parameter

--- 2-enum/mip/turbo-mip.py
+++ 2-enum/mip/turbo-mip.py
@@ -1,6 +1,6 @@
 from mip import *
 
-m = Model(sense='MAX', solver_name=CBC)
+m = Model(solver_name=CBC)
 
 ts = m.add_var_tensor((3, 5), 'turbines')
 
@@ -16,4 +16,5 @@ for j, N in enumerate(Y):
   m.add_constr(sum(tss[:, j]) <= N, f'Dist_{j}')
 
 m.objective = ts.sum()
+m.sense = MAX
 m.write('turbo.mip.lp')
