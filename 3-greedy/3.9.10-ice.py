94698ecf1d7bf19679417d15da0a9420eb144927
Stas Ukolov <ukoloff@gmail.com>
2022-08-02 20:05:50 +0500

    Almost correct solution

--- 3-greedy/3.9.10-ice.py
+++ 3-greedy/3.9.10-ice.py
@@ -1,4 +1,14 @@
-f = open("ice.in")
+f = open("ice2.in")
 
 names = [f.readline().strip() for i in range(int(f.readline()))]
-print(*names)
+
+count = 1
+seen = set()
+for name in names:
+    if name not in seen:
+        seen.add(name)
+        continue
+    count += 1
+    seen.clear()
+
+print(count)
