4044e3c29fc7a9649101da637912f98fccdc208c
Stas Ukolov <ukoloff@gmail.com>
2022-08-04 18:09:02 +0500

    Solve Bug problem

--- 4-dynprog/4.2.3-tile3.py
+++ 4-dynprog/4.2.3-tile3.py
@@ -1,8 +1,9 @@
 def tiles3(n):
-  a, b, c = 1, 1, 1
-  for i in range(n):
-    a, b, c = b, c, a + c
-  return a
+    a, b, c = 1, 1, 1
+    for i in range(n):
+        a, b, c = b, c, a + c
+    return a
+
 
 for i in range(21):
-  print(i, tiles3(i))
+    print(i, tiles3(i))
---
+++ 4-dynprog/4.3.3-bug.py
@@ -0,0 +1,40 @@
+f = open('bug.in')
+costs = [[int(z) for z in line.split()]for line in f]
+
+
+def gather(costs):
+    dirs = []
+    bests = []
+    prev = None
+    for i, row in enumerate(costs):
+        dir = []
+        dirs.append(dir)
+        sums = []
+        bests.append(sums)
+        for j, cell in enumerate(row):
+            options = []
+            if i > 0:
+                options.append([prev[j], 'D'])
+            if j > 0:
+                options.append([sums[-1], 'R'])
+            if len(options) == 0:
+                options.append([0, '.'])
+            z = max(options)
+            sums.append(z[0] + costs[i][j])
+            dir.append(z[1])
+        prev = sums
+    i, j = -1, -1
+    path = []
+    while True:
+        c = dirs[i][j]
+        if c == 'R':
+            j -= 1
+        elif c == 'D':
+            i -= 1
+        else:
+            break
+        path.append(c)
+    return ''.join(reversed(path))
+
+
+print(gather(costs))
