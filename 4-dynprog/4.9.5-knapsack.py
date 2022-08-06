a23669e58253f1b4e351ed214eebea55093a140f
Stas Ukolov <ukoloff@gmail.com>
2022-08-06 16:21:10 +0500

    Refactor

--- 4-dynprog/4.9.5-knapsack.py
+++ 4-dynprog/4.9.5-knapsack.py
@@ -11,24 +11,18 @@ items = [readi() for i in range(n)]
 
 def knapsack(W, items):
     z = [0] * (W + 1)
-    last = [None] * (W + 1)
+    paths = [None] * (W + 1)
     for item, (w, c) in enumerate(items):
         for i in range(W, w - 1, -1):
             if z[i] < z[i - w] + c:
                 z[i] = z[i - w] + c
                 # last[i] = item
-                prev = last[i - w]
+                prev = paths[i - w]
                 if prev is None:
                   prev = []
-                last[i] = [*prev, item + 1]
-    # result = []
-    # i = W
-    # while i > 0 and last[i] >= 0:
-    #   result.append(last[i] + 1)
-    #   i -= items[last[i]][0]
-    # result.reverse()
+                paths[i] = [*prev, item + 1]
     print(z[W])
-    print(*last[W])
+    print(*paths[W])
 
 
 knapsack(W, items)
