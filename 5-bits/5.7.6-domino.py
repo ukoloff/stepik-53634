2f9e9b6090ea6c144cd89d1895fa4bd472f3127f
Stas Ukolov <ukoloff@gmail.com>
2022-08-06 21:00:09 +0500

    Solve Domino

--- 5-bits/5.7.6-domino.py
+++ 5-bits/5.7.6-domino.py
@@ -21,5 +21,29 @@ def bits2(n):
     return table
 
 
-for i in range(1, 5):
-    print(*(f"{'+' if ok else '-'}{n:0{i}b}" for n, ok in enumerate(bits2(i))))
+def tile(n, m, k=0):
+    if n > m:
+        n, m = m, n
+    b2 = bits2(n)
+
+    counts = [0] * 2 ** n
+    counts[0] = 1
+
+    for i in range(m):
+        layer = [0] * len(counts)
+        for p, N in enumerate(counts):
+            if N == 0:
+                continue
+            for q in range(len(layer)):
+                if p & q == 0 and b2[p ^ q]:
+                    layer[q] += N
+                    if k != 0:
+                      layer[q] %= k
+        counts = layer
+    return counts[0]
+
+
+print(tile(3, 2))
+print(tile(5, 6))
+print(tile(5000, 6) % 1000000000)
+# print(tile(16, 16, 1000000000))
