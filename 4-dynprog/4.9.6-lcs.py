8444097bd6eb3e7e4af59bf942d3a02a68ae1229
Stas Ukolov <ukoloff@gmail.com>
2022-08-06 16:26:13 +0500

    Solve LCS

--- 4-dynprog/4.9.6-lcs.py
+++ 4-dynprog/4.9.6-lcs.py
@@ -1,4 +1,5 @@
-f = open('seq0.in')
+import numpy as np
+f = open('seq2.in')
 
 
 def read():
@@ -10,4 +11,12 @@ a, b = read(), read()
 
 
 def lcs(a, b):
-    ...
+    Z = np.zeros((len(a) + 1, len(b) + 1), dtype=int)
+    for i, ca in enumerate(a):
+      for j, cb in enumerate(b):
+        Z[i+1, j+1] = max(Z[i, j+1], Z[i+1, j])
+        if ca == cb:
+          Z[i+1, j+1] = max(Z[i+1, j+1], Z[i, j] + 1)
+    return Z[-1, -1]
+
+print(lcs(a, b))
