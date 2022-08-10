7ce00f29509fc6edf6fa24e1d6d9d1099db14af1
Stas Ukolov <ukoloff@gmail.com>
2022-08-10 20:29:03 +0500

    Solve Time problem

--- 6-final/6.1.10-time.py
+++ 6-final/6.1.10-time.py
@@ -1,6 +1,13 @@
-f = open('time0.in')
+f = open('time2.in')
 
 todos = [[int(z) for z in f.readline().split()]
          for i in range(int(f.readline()))]
 
-print(todos)
+opt = {0: 0}
+
+for t, d in todos:
+    for n, finish in [(k+1, v+t) for k, v in opt.items() if v <= d]:
+        if n not in opt or finish < opt[n]:
+            opt[n] = finish
+
+print(max(opt.keys()))
