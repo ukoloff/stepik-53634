e26742aa1b8044c89443cc02e6bd312925f320d5
Stas Ukolov <ukoloff@gmail.com>
2022-08-10 19:57:55 +0500

    Solve Gold split

--- 6-final/6.1.6-gold.py
+++ 6-final/6.1.6-gold.py
@@ -1,5 +1,14 @@
-f = open('gold-0.in')
+f = open('gold3.in')
 
 f.readline()
 bars = [int(z) for z in f.readline().split()]
 
+S = sum(bars)
+UB = S // 2
+
+sums = {0}
+for bar in bars:
+  sums.update([i + bar for i in sums if i + bar <= UB])
+
+delta = S - 2 * max(sums)
+print(delta)
