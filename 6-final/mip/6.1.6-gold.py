4bbf5eb6590dbea4f1572cf06c68212e0063659c
Stas Ukolov <ukoloff@gmail.com>
2022-08-12 20:22:37 +0500

    Try to fix gold3 solution

--- 6-final/mip/6.1.6-gold.py
+++ 6-final/mip/6.1.6-gold.py
@@ -4,15 +4,18 @@ import numpy as np
 f = open('../gold3.in')
 
 f.readline()
-bars = np.array([int(z) for z in f.readline().split()])
+bars = np.array([int(z) for z in f.readline().split()], dtype=np.int64)
 
 m = Model(name='Gold')
 m.init_numpy()
+m.round_solution = True
 
 X = np.array(m.binary_var_list(len(bars), name='x'))
 
 m.minimize(m.abs(bars.sum() - 2 * (bars * X).sum()))
 
 sol = m.solve()
-sol.display()
-print(*(z.sv for z in X))
+# sol.display()
+Xx = np.array([int(z.sv) for z in X])
+print(Xx)
+print(bars.sum() - 2 * (bars * Xx).sum())
